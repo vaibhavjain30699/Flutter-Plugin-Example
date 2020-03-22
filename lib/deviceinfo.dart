77264b66fe8eeb3bdaff49b141e6e6d8c81a4e3d
Vaibhav Jain <vaibhavjain30699@gmail.com>
2020-03-23 02:29:52 +0530

    Added more comments

--- lib/deviceinfo.dart
+++ lib/deviceinfo.dart
@@ -5,11 +5,13 @@ import 'package:flutter/services.dart';
 class Deviceinfo {
   static const MethodChannel _channel = const MethodChannel('deviceinfo');
 
+  // for fetching the device name asynchronously
   static Future<String> get DeviceName async {
     final String name = await _channel.invokeMethod('getDeviceName');
     return name;
   }
 
+  // for fetching the battery level asynchronously
   static Future<int> get BatteryLevel async {
     final int battery = await _channel.invokeMethod('getBatteryLevel');
     return battery;
