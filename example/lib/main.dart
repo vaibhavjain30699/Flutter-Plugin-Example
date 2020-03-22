cb13060864a1d6c81f00f71aa85fe5ab4f102a74
Vaibhav Jain <vaibhavjain30699@gmail.com>
2020-03-22 11:14:29 +0530

    Formatterd code using dartfmt

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,39 +20,39 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initBatteryLevel();  //function to get the battery level percentage
-    initDeviceName();    //function to get the device name
+    initBatteryLevel(); //function to get the battery level percentage
+    initDeviceName(); //function to get the device name
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initDeviceName() async{
+  Future<void> initDeviceName() async {
     String name;
     //Platform messages may fail, so we use a PlatformException try/catch
-    try{
+    try {
       name = await Deviceinfo.DeviceName;
-    }on PlatformException{
+    } on PlatformException {
       name = "Failed to get Device Name";
     }
 
     // If the widget was not removed from the tree while the asynchronous platform
     // message was in flight, we want to call the setState
     // to update our existent appearance.
-    if(mounted){
+    if (mounted) {
       setState(() {
         _deviceName = name;
       });
     }
   }
 
-  Future<void> initBatteryLevel() async{
+  Future<void> initBatteryLevel() async {
     int battery;
-    try{
+    try {
       battery = await Deviceinfo.BatteryLevel;
-    }on PlatformException{
+    } on PlatformException {
       battery = -1;
     }
 
-    if(mounted){
+    if (mounted) {
       setState(() {
         _batteryLevel = battery;
       });
@@ -67,19 +67,21 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Device Info'),
         ),
         body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text ("$_deviceName",
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "$_deviceName",
               style: TextStyle(
                 fontSize: 35,
-              ),),
-              Text ("Batter Level : $_batteryLevel",
-              style: TextStyle(
-                fontSize: 20
-              ),)
-            ],
-          )
-        ),
+              ),
+            ),
+            Text(
+              "Batter Level : $_batteryLevel",
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        )),
       ),
     );
   }
